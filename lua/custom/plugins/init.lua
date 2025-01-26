49550c01c0dabe5eba11d7a1652bdd1755e77ff3
Siphosakhe2583003 <siphosakhemkhwanazi.github@gmail.com>
2025-01-26 20:56:59 +0200

    my nvim config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,17 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+local plugins = {
+  {
+    'olexsmir/gopher.nvim',
+    ft = 'go',
+    config = function(_, opts)
+      require('gopher').setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
+return plugins
